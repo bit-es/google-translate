887f031c05ca1c1e4bce7ae6c63ccc62b82c4f12
Faros <114036707+a-faros@users.noreply.github.com>
2026-06-25 12:57:15 +0800

    Update language-switch.blade.php

--- resources/views/language-switch.blade.php
+++ resources/views/language-switch.blade.php
@@ -1,20 +1,18 @@
-<div class="flex w-full justify-end items-center mb-4">
-    <div class="inline-block">
-        <x-filament::dropdown placement="bottom-end">
-            <x-slot name="trigger">
-                <x-filament::icon-button icon="bites-lang" label="Languange Translator" />
-            </x-slot>
+<div id="helpers-translate">
+    <x-filament::dropdown placement="bottom-end">
+        <x-slot name="trigger">
+            <x-filament::icon-button icon="bites-lang" label="Languange Translator" />
+        </x-slot>
 
-            <x-filament::dropdown.list>
-                @foreach (\Bites\GoogleTranslate\Enums\Language::cases() as $language)
-                    <x-filament::dropdown.list.item :icon="$language->getIcon()"
-                        x-on:click="triggerGoogleTranslate('{{ $language->value }}') && close();">
-                        {{ $language->getLabel() }}
-                    </x-filament::dropdown.list.item>
-                @endforeach
-                <div id="google_translate_element"></div>
+        <x-filament::dropdown.list>
+            @foreach (\Bites\GoogleTranslate\Enums\Language::cases() as $language)
+                <x-filament::dropdown.list.item :icon="$language->getIcon()"
+                    x-on:click="triggerGoogleTranslate('{{ $language->value }}') && close();">
+                    {{ $language->getLabel() }}
+                </x-filament::dropdown.list.item>
+            @endforeach
+            <div id="google_translate_element"></div>
 
-            </x-filament::dropdown.list>
-        </x-filament::dropdown>
-    </div>
+        </x-filament::dropdown.list>
+    </x-filament::dropdown>
 </div>
